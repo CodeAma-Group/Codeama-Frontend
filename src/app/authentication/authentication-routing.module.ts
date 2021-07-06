806922a8795a7ada4ca6622aca11e04fec46a1cf
abi-seth <abiheloaf@gmail.com>
2021-07-06 18:26:05 +0200

    Feat: authentication routing and module configuration done

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,7 +1,51 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ModuleoutletComponent } from './moduleoutlet/moduleoutlet.component'
+import { LoginformComponent } from './loginform/loginform.component'
+import { SignupformComponent } from './signupform/signupform.component'
+import { LoginfacerecoComponent } from './loginfacereco/loginfacereco.component'
+import { SignupfacerecoComponent } from './signupfacereco/signupfacereco.component'
+
+const routes: Routes = [
+
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    component: ModuleoutletComponent,
+    children: [
+
+      {
+        path: '',
+        redirectTo: 'login', 
+        pathMatch: 'full'
+      },
+      {
+        path: 'login',
+        component: LoginformComponent
+      },
+      {
+        path: 'signup',
+        component: SignupformComponent
+      },
+      {
+        path: 'Loginfacerecongtn',
+        component: LoginfacerecoComponent
+      },
+      {
+        path: 'signupfacerecongtn',
+        component: SignupfacerecoComponent
+      }
+
+    ]
+  }
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,10 +3,20 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { ModuleoutletComponent } from './moduleoutlet/moduleoutlet.component';
+import { LoginformComponent } from './loginform/loginform.component';
+import { SignupformComponent } from './signupform/signupform.component';
+import { SignupfacerecoComponent } from './signupfacereco/signupfacereco.component';
+import { LoginfacerecoComponent } from './loginfacereco/loginfacereco.component';
 
 
 @NgModule({
-  declarations: [ModuleoutletComponent],
+  declarations: [
+    ModuleoutletComponent, 
+    LoginformComponent, 
+    SignupformComponent, 
+    SignupfacerecoComponent, 
+    LoginfacerecoComponent
+  ],
   imports: [
     CommonModule,
     AuthenticationRoutingModule
