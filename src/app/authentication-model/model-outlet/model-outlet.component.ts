521280c87edeaa38c1bff5d7f245c1822311a990
abi-seth <abiheloaf@gmail.com>
2021-07-06 17:26:08 +0200

    Fix: fixed git modification errors

---
+++ src/app/authentication-model/model-outlet/model-outlet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-model-outlet',
+  templateUrl: './model-outlet.component.html',
+  styleUrls: ['./model-outlet.component.css']
+})
+export class ModelOutletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
