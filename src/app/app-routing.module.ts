8812cdd89d4afefcbf1614bf2d0b8d9735916014
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-06 10:12:05 -1200

    added link in app-routing module and in navbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutUsComponent} from './about-us/about-us.component';
+import {ChallengesComponent} from './challenges/challenges.component';
+import {CodeAmasComponent} from './code-amas/code-amas.component';
+import {NotificationsComponent} from './notifications/notifications.component'
+import {ProfileComponent} from './profile/profile.component'
+import {HoodComponent} from './hood/hood.component'
+import {SupportComponent} from './support/support.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"hood", component:HoodComponent},
+  {path:"profile", component:ProfileComponent},
+  {path:"notifications", component:NotificationsComponent},
+  {path:"challenges", component:ChallengesComponent},
+  {path:"codeAmas", component:CodeAmasComponent},
+  {path:"aboutUs", component:AboutUsComponent},
+  {path:"support", component:SupportComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
