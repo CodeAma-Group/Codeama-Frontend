caeebaf7283e324c1a1aeff53ab0c1bad9ffa9f7
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-06 09:33:27 -1200

    added different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { HoodComponent } from './hood/hood.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+import { CodeAmasComponent } from './code-amas/code-amas.component';
+import { SupportComponent } from './support/support.component';
+import { ProfileComponent } from './profile/profile.component';
+import { NotificationsComponent } from './notifications/notifications.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SidebarComponent
+    SidebarComponent,
+    HoodComponent,
+    AboutUsComponent,
+    ChallengesComponent,
+    CodeAmasComponent,
+    SupportComponent,
+    ProfileComponent,
+    NotificationsComponent
   ],
   imports: [
     BrowserModule,
