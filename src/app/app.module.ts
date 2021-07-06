fc993b59cd6cbc4b34836931b3284c57875a0f62
abi-seth <abiheloaf@gmail.com>
2021-07-06 17:06:04 +0200

    New: modified everything to remove logical bugs in routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { HoodComponent } from './hood/hood.component';
-import { AboutUsComponent } from './about-us/about-us.component';
-import { ChallengesComponent } from './challenges/challenges.component';
-import { CodeAmasComponent } from './code-amas/code-amas.component';
-import { SupportComponent } from './support/support.component';
-import { ProfileComponent } from './profile/profile.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { HeaderComponent } from './components/header/header.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    HoodComponent,
-    AboutUsComponent,
-    ChallengesComponent,
-    CodeAmasComponent,
-    SupportComponent,
-    ProfileComponent,
-    NotificationsComponent,
-    HeaderComponent,
     PageNotFoundComponent
   ],
   imports: [
