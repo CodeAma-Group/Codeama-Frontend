5c47e838d6cad2012587b262c622bf0ed84a80b6
Esther Umuhoza <eumuhoza83@gmail.com>
2021-07-08 16:24:55 +0200

    Amas Profile modifications

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,6 +32,9 @@ const routes: Routes = [
     component: CodeamasComponent,
     children: [  
       {
+        path: '', component: ProfileComponent
+      },
+      {
         path: 'profile', component: ProfileComponent
       },
     ]
