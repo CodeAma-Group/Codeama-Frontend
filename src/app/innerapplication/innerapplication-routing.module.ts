0d2d2c69595d5ee9d83c1259064528e636218fb9
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-07 21:23:58 -1200

    changes

--- src/app/innerapplication/innerapplication-routing.module.ts
+++ src/app/innerapplication/innerapplication-routing.module.ts
@@ -1,68 +1,71 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ModuleoutletComponent } from './moduleoutlet/moduleoutlet.component'
-import { HoodComponent } from './hood/hood.component'
-import { ProjectsComponent } from './projects/projects.component'
-import { ChallengesComponent } from './challenges/challenges.component'
-import { CodeamasComponent } from './codeamas/codeamas.component'
-import { NotificationsComponent } from './notifications/notifications.component'
-import { SupportComponent } from './support/support.component'
-import { ProfileComponent } from './profile/profile.component'
+import { ModuleoutletComponent } from './moduleoutlet/moduleoutlet.component';
+import { HoodComponent } from './hood/hood.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+import { CodeamasComponent } from './codeamas/codeamas.component';
+import { NotificationsComponent } from './notifications/notifications.component';
+import { SupportComponent } from './support/support.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProjectDetailsComponent } from './projects/project-details/project-details.component';
 
 const routes: Routes = [
-
   {
     path: '',
     redirectTo: 'app',
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
   {
     path: 'app',
     component: ModuleoutletComponent,
     children: [
-
       {
         path: '',
         redirectTo: 'hood',
-        pathMatch: 'full'
+        pathMatch: 'full',
       },
       {
         path: 'hood',
-        component: HoodComponent
+        component: HoodComponent,
       },
       {
         path: 'projects',
-        component: ProjectsComponent
+        component: ProjectsComponent,
       },
       {
         path: 'challenges',
-        component: ChallengesComponent
+        component: ChallengesComponent,
+        children: [
+          {
+            path: 'projectDetails',
+            component: ProjectDetailsComponent,
+          },
+        ],
       },
       {
         path: 'codeamas',
-        component: CodeamasComponent
+        component: CodeamasComponent,
       },
       {
         path: 'notifications',
-        component: NotificationsComponent
+        component: NotificationsComponent,
       },
       {
         path: 'support',
-        component: SupportComponent
+        component: SupportComponent,
       },
       {
         path: 'profile',
-        component: ProfileComponent
-      }
-
-    ]
-  }
-
+        component: ProfileComponent,
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class InnerapplicationRoutingModule { }
+export class InnerapplicationRoutingModule {}
