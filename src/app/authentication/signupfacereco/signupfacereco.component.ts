23de91e54650393058f0603b7a5118625bba4fc8
abi-seth <abiheloaf@gmail.com>
2021-07-09 15:22:36 +0200

    Feat: implemented full authentication page with face recognition

--- src/app/authentication/signupfacereco/signupfacereco.component.ts
+++ src/app/authentication/signupfacereco/signupfacereco.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { WebcamImage } from 'ngx-webcam';
+
 @Component({
   selector: 'app-signupfacereco',
   templateUrl: './signupfacereco.component.html',
@@ -10,6 +12,20 @@ export class SignupfacerecoComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    document.getElementById("cam").style.display = "none"
+
+    setInterval(() => {
+      document.getElementById("cam").style.display = "block"
+      document.getElementById("sample").style.display = "none"
+    }, 5000)
+
+  }
+
+  webcamImage: WebcamImage = null;
+
+  handleImage(webcamImage: WebcamImage) {
+    this.webcamImage = webcamImage;
   }
 
 }
