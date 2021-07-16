2fce104631272bd65db3617ccebc221d42b51d40
abi-seth <abiheloaf@gmail.com>
2021-07-16 13:02:38 +0200

    Fix: added auth interceptors to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,11 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
+
+import { AuthService } from './authentication/_authServices/auth.service'
+import { AuthGuard } from './authentication/common/auth.guard'
+import { AuthinterceptorService } from './authentication/_authServices/authinterceptor.service'
 
 
 @NgModule({
@@ -20,6 +24,15 @@ import { HttpClientModule } from '@angular/common/http'
     AppRoutingModule,
     FormsModule,
     HttpClientModule
+  ],
+  providers: [
+    AuthService,
+		AuthGuard,
+		{
+			provide: HTTP_INTERCEPTORS,
+			useClass: AuthinterceptorService,
+			multi:true
+		}
   ],
   bootstrap: [AppComponent]
 })
