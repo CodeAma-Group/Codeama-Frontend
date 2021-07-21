d65263f0d657de87c35d6a59da18f335c24dc5ae
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-20 18:24:21 -1200

    ..

--- src/app/innerapplication/project-registration/project-registration.component.ts
+++ src/app/innerapplication/project-registration/project-registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder} from '@angular/forms'
+import { FormBuilder } from '@angular/forms';
 @Component({
   selector: 'app-project-registration',
   templateUrl: './project-registration.component.html',
@@ -7,48 +7,48 @@ import {FormBuilder} from '@angular/forms'
 })
 export class ProjectRegistrationComponent implements OnInit {
   selectedImg = null;
-  OtherImages=[];
-  imgUrl: string ="";
-  videoUrl:string="../../../assets/images/video.mp4"
-  // ../../../assets/images/image1.png
-  question
+  OthernewImages = [];
+  imgUrl: string = '';
+  videoUrl: string = '../../../assets/newImages/video.mp4';
+  newImageUrl: string = '';
+  // ../../../assets/newImages/newImage1.png
+  question;
   constructor(private formBuilder: FormBuilder) {
     this.question = this.formBuilder.group({
-      title: ["",[]],
-      desc: ["",[]],
-      qtn: ["...",[]],
-      tagged_tech: ["",[]]
-    })
-   }
-   public options = [
-     {label: "Java", value:"Java"},
-     {label: "Javascript", value:"Javascript"},
-     {label: "Vuex", value:"Vuex"},
-     {label: "React", value:"React"},
-     {label: "Rust", value:"Rust"},
-     {label: "C", value:"C"},
-     {label: "C++", value:"C++"},
-     {label: "C#", value:"C#"},
-     {label: "Go", value:"Go"},
-     {label: "HTML", value:"HTML"},
-     {label: "CSS", value:"CSS"},
-     {label: "SCSS", value:"SCSS"},
-     {label: "SASS", value:"SASS"},
-     {label: "Angular", value:"Angular"},
-     {label: "Python", value:"Python"},
-     {label: "Svelte", value:"Svelte"},
-     {label: "Typescript", value:"Typescript"},
-     {label: "Ruby", value:"Ruby"},
-     {label: "Haskell", value:"Haskell"},
-     {label: "NextJs", value:"NextJs"},
-     {label: "Php", value:"Php"},
-   ]
-   public fields = {text: 'label',value: 'value'}
-
+      title: ['', []],
+      desc: ['', []],
+      qtn: ['...', []],
+      tagged_tech: ['', []],
+    });
+  }
+  public options = [
+    { label: 'Java', value: 'Java' },
+    { label: 'Javascript', value: 'Javascript' },
+    { label: 'Vuex', value: 'Vuex' },
+    { label: 'React', value: 'React' },
+    { label: 'Rust', value: 'Rust' },
+    { label: 'C', value: 'C' },
+    { label: 'C++', value: 'C++' },
+    { label: 'C#', value: 'C#' },
+    { label: 'Go', value: 'Go' },
+    { label: 'HTML', value: 'HTML' },
+    { label: 'CSS', value: 'CSS' },
+    { label: 'SCSS', value: 'SCSS' },
+    { label: 'SASS', value: 'SASS' },
+    { label: 'Angular', value: 'Angular' },
+    { label: 'Python', value: 'Python' },
+    { label: 'Svelte', value: 'Svelte' },
+    { label: 'Typescript', value: 'Typescript' },
+    { label: 'Ruby', value: 'Ruby' },
+    { label: 'Haskell', value: 'Haskell' },
+    { label: 'NextJs', value: 'NextJs' },
+    { label: 'Php', value: 'Php' },
+  ];
+  public fields = { text: 'label', value: 'value' };
 
   fileSelected(event) {
     this.selectedImg = event.target.files[0].name;
-    console.log(event.timeStamp+this.selectedImg);
+    console.log(event.timeStamp + this.selectedImg);
 
     if (event.target.files) {
       const reader = new FileReader();
@@ -60,84 +60,95 @@ export class ProjectRegistrationComponent implements OnInit {
     }
   }
 
-  multipleProjectImages(event){
-    console.log(this.OtherImages = event.target.files[0].name);
-    
+  multipleProjectImages(event) {
+    this.OthernewImages = event.target.files;
+    if (this.OthernewImages) {
+      [].forEach.call(this.OthernewImages, readAndPreview);
+    }
+    function readAndPreview(file) {
+      // Make sure `file.name` matches our extensions criteria
+      if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+        return alert(file.name + ' is not an newImage');
+      } // else...
+
+      var reader = new FileReader();
+      reader.readAsDataURL(file);
+      reader.onload = (event: any) => {
+        this.newImageUrl=event.target.result
+      }
+      console.log(this.newImageUrl);
+      
+    }
   }
   uploadFile() {
     const fd = new FormData();
     fd.append('file', this.selectedImg, this.selectedImg.name);
   }
-  
 
-  cookieVal:string = "";
+  cookieVal: string = '';
 
   ngOnInit(): void {
+    var cookieName = 'isDark';
 
-    var cookieName = "isDark";
-
-    var matchCookie = document.cookie.match(new RegExp('(^| )' + cookieName + '=([^;]+)'));
+    var matchCookie = document.cookie.match(
+      new RegExp('(^| )' + cookieName + '=([^;]+)')
+    );
     if (matchCookie) {
       this.cookieVal = matchCookie[2];
     }
 
-    if (this.cookieVal == "") {
-      this.cookieVal = "false"
+    if (this.cookieVal == '') {
+      this.cookieVal = 'false';
     } else {
-
-      if (this.cookieVal == "false") {
+      if (this.cookieVal == 'false') {
         let root = document.documentElement;
-        root.style.setProperty('--pureWhite', '--pureWhite')
-        root.style.setProperty('--mainBlack', '--pureWhite')   
-        root.style.setProperty('--dimBlue', '#66A5F0')
-        root.style.setProperty('--dimerBlue', '#f2f6fa')
-        root.style.setProperty('--mainBlack', '#17202A')
-        root.style.setProperty('--inputWhite', '#ffffff')     
-        
+        root.style.setProperty('--pureWhite', '--pureWhite');
+        root.style.setProperty('--mainBlack', '--pureWhite');
+        root.style.setProperty('--dimBlue', '#66A5F0');
+        root.style.setProperty('--dimerBlue', '#f2f6fa');
+        root.style.setProperty('--mainBlack', '#17202A');
+        root.style.setProperty('--inputWhite', '#ffffff');
       } else {
         let root = document.documentElement;
-        root.style.setProperty('--modeDimBlue', '--dimBlue')
-        root.style.setProperty('--pureWhite', '#0D0E1A')
-        root.style.setProperty('--dimBlue', '#1D2747')
-        root.style.setProperty('--dimerBlue', '#0F111E')
-        root.style.setProperty('--mainBlack', '#ffffff')
-        root.style.setProperty('--inputWhite', '#141B30')
-
+        root.style.setProperty('--modeDimBlue', '--dimBlue');
+        root.style.setProperty('--pureWhite', '#0D0E1A');
+        root.style.setProperty('--dimBlue', '#1D2747');
+        root.style.setProperty('--dimerBlue', '#0F111E');
+        root.style.setProperty('--mainBlack', '#ffffff');
+        root.style.setProperty('--inputWhite', '#141B30');
       }
-
     }
-
   }
 
-
   async modeChange() {
-    var cookieName = "isDark";
-    console.log("yooo")
-    var matchCookie = await document.cookie.match(new RegExp('(^| )' + cookieName + '=([^;]+)'));
+    var cookieName = 'isDark';
+    console.log('yooo');
+    var matchCookie = await document.cookie.match(
+      new RegExp('(^| )' + cookieName + '=([^;]+)')
+    );
     if (matchCookie) {
       this.cookieVal = matchCookie[2];
-      console.log(matchCookie[2])
-    }
-    else{
+      console.log(matchCookie[2]);
+    } else {
       // document.cookie = "isDark=false; path=/; max-age=" + 365*24*60*60;
       // this.cookieVal = "false";
-      alert("Make sure your cookies are enabled because we use cookies to keep track of your settings on the devices!")
+      alert(
+        'Make sure your cookies are enabled because we use cookies to keep track of your settings on the devices!'
+      );
     }
 
-    if (this.cookieVal == "true") {
+    if (this.cookieVal == 'true') {
       let root = document.documentElement;
-      root.style.setProperty('--pureWhite', '--pureWhite')
-      root.style.setProperty('--mainBlack', '--pureWhite')
+      root.style.setProperty('--pureWhite', '--pureWhite');
+      root.style.setProperty('--mainBlack', '--pureWhite');
 
-      document.cookie = "isDark=false; path=/; max-age=" + 365*24*60*60;
+      document.cookie = 'isDark=false; path=/; max-age=' + 365 * 24 * 60 * 60;
     } else {
       let root = document.documentElement;
-      root.style.setProperty('--pureWhite', '#0D0E1A')
-      root.style.setProperty('--mainBlack', '#ffffff')
+      root.style.setProperty('--pureWhite', '#0D0E1A');
+      root.style.setProperty('--mainBlack', '#ffffff');
 
-      document.cookie = "isDark=true; path=/; max-age=" + 365*24*60*60;
+      document.cookie = 'isDark=true; path=/; max-age=' + 365 * 24 * 60 * 60;
     }
-
-}
-
+  }
 }
