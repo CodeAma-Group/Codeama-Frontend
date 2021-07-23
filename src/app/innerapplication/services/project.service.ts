1ec359b2211119fcb61fad48f4532aa604a3504d
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-22 19:29:36 -1200

    ..project calling back

--- src/app/innerapplication/services/project.service.ts
+++ src/app/innerapplication/services/project.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProjectService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
+  saveProject(data) {
+    return this.http.post('https://codeama-backend.herokuapp.com/project', data);
+  }
 }
