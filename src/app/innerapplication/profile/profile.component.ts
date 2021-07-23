dbf0709f501c4daf14a7a1a392b97258b76a6db8
abi-seth <abiheloaf@gmail.com>
2021-07-23 10:32:47 +0200

    Fix: fixed dark modes errors

--- src/app/innerapplication/profile/profile.component.ts
+++ src/app/innerapplication/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AppsettingsService } from '../../appsettings.service'
+
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -7,9 +9,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor( private _settings: AppsettingsService ) { }
 
   ngOnInit(): void {
+    this._settings.settings();
   }
 
 
