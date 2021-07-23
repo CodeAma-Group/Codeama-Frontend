1b7741fb4f55f5988a76f80224d61ba8302f06c5
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-22 14:15:06 -1200

    .. added form add-bugcomponent

--- src/app/innerapplication/dashboard/add-bug/add-bug.component.ts
+++ src/app/innerapplication/dashboard/add-bug/add-bug.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import{FormControl,FormGroup} from '@angular/forms'
 @Component({
   selector: 'app-add-bug',
   templateUrl: './add-bug.component.html',
   styleUrls: ['./add-bug.component.css']
 })
 export class AddBugComponent implements OnInit {
-
   constructor() { }
+
+newBugForm= new FormGroup({
+bugTitle: new FormControl(''),
+bugTechnology:new FormControl(''),
+bugDetails: new FormControl(''),
+bugDescription:new FormControl(),
+
+})
 
   ngOnInit(): void {
   }
-
+collectData(){
+  console.log(this.newBugForm.value);
+  
+}
 }
