7c6030fd695d0799b5952e99022adc968bbdb52f
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-23 16:27:41 -1200

    ...

--- src/app/innerapplication/project-details/project-details.component.ts
+++ src/app/innerapplication/project-details/project-details.component.ts
@@ -1,16 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute } from '@angular/router';
+import { ProjectService } from '../services/project.service';
 @Component({
   selector: 'app-project-details',
   templateUrl: './project-details.component.html',
   styleUrls: ['./project-details.component.css'],
 })
 export class ProjectDetailsComponent implements OnInit {
-  constructor() {}
+  constructor(
+    private router: ActivatedRoute,
+    private project: ProjectService
+  ) {}
   projectImages = [];
   ProjectImageOnStage: string = '';
-
+  projectData: any;
   ngOnInit(): void {
+    this.project.getProject(this.router.snapshot.params.id).subscribe((res) => {
+      this.projectData = res;
+      this.projectData = this.projectData.data;
+    });
+
     this.projectImages = [
       '../../../assets/images/image1.png',
       '../../../assets/images/image2.png',
@@ -20,7 +29,7 @@ export class ProjectDetailsComponent implements OnInit {
     ];
     this.ProjectImageOnStage = this.projectImages[0];
   }
-  imageOnStage(data:any) {
-    this.ProjectImageOnStage=data.src;
+  imageOnStage(data: any) {
+    this.ProjectImageOnStage = data.src;
   }
 }
