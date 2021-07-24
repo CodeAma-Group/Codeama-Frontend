d94fcacde1df9401dd5bde611e8da4855aa51e2c
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-23 15:26:26 -1200

    ...

--- src/app/innerapplication/projects/projects.component.ts
+++ src/app/innerapplication/projects/projects.component.ts
@@ -9,13 +9,13 @@ export class ProjectsComponent implements OnInit {
 
   constructor(private projects:ProjectService) { }
   id=10;
+  url="https://codeama-backend.herokuapp.com/project"
   show:boolean=false
   projectsData:any
   ngOnInit(): void {
     this.projects.getProjects().subscribe((res)=>{
       this.projectsData=res
       console.log(this.projectsData.data);
-      
       this.projectsData=this.projectsData.data;
       this.show=true
     })
