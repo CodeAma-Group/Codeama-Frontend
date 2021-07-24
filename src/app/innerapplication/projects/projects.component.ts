79606dfbf86feae761deb479a1280a2b0f2c0659
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-07-23 13:42:22 -1200

    ..

--- src/app/innerapplication/projects/projects.component.ts
+++ src/app/innerapplication/projects/projects.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProjectService } from '../services/project.service';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private projects:ProjectService) { }
   id=10;
+  projectsData={}
   ngOnInit(): void {
+    this.projects.getProjects().subscribe((res)=>{
+      console.log(res);
+      this.projectsData=res
+    })
   }
 
 }
