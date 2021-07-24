70f843ddd4d5bb53e8538f3fc51abf649084095c
A. Belard <angebelard@Gmail.com>
2021-07-24 03:54:34 +0200

    Update sidebar.component.ts

--- src/app/innerapplication/sidebar/sidebar.component.ts
+++ src/app/innerapplication/sidebar/sidebar.component.ts
@@ -24,6 +24,7 @@ export class SidebarComponent implements OnInit {
       this.userId = this.userData.data._id;
     },
     err => {
+      console.log(err)
       alert("Token expired!");
       this.authService.logout();
       this.router.navigate(['auth'])
