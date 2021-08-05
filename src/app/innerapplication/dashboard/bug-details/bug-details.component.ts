56c2e465e6a987a9211efb8cbfb4a98d9fa0971b
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-04 22:52:31 -1200

    .

--- src/app/innerapplication/dashboard/bug-details/bug-details.component.ts
+++ src/app/innerapplication/dashboard/bug-details/bug-details.component.ts
@@ -3,13 +3,32 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bug-details',
   templateUrl: './bug-details.component.html',
-  styleUrls: ['./bug-details.component.css']
+  styleUrls: ['./bug-details.component.css'],
 })
 export class BugDetailsComponent implements OnInit {
+  constructor() {}
+  htmlContent = '';
 
-  constructor() { }
+  codeMirrorOptions: any = {
+    theme: 'default',
+    mode: 'application/ld+json',
+    lineNumbers: true,
+    readOnly:true,
+    autocorrect: true,
+    smartIndent: true,
+    lineWrapping: true,
+    readonly: true,
+    foldGutter: true,
+    gutters: [
+      'CodeMirror-linenumbers',
+      'CodeMirror-foldgutter',
+      'CodeMirror-lint-markers',
+    ],
+    autoCloseBrackets: true,
+    automatically: true,
+    matchBrackets: true,
+    lint: true,
+  };
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
