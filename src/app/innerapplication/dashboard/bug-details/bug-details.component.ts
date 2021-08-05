82acdfa6672cd5bcc496b91a9679a8794cb8684c
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-04 23:45:59 -1200

    ...

--- src/app/innerapplication/dashboard/bug-details/bug-details.component.ts
+++ src/app/innerapplication/dashboard/bug-details/bug-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
 
 @Component({
   selector: 'app-bug-details',
@@ -8,6 +9,40 @@ import { Component, OnInit } from '@angular/core';
 export class BugDetailsComponent implements OnInit {
   constructor() {}
   htmlContent = '';
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Poppins',
+    defaultFontSize: '2',
+    fonts: [
+      { class: 'arial', name: 'Arial' },
+      { class: 'Roboto', name: 'Roboto' },
+      { class: 'Poppins', name: 'Poppins' },
+      { class: 'comic-sans-ms', name: 'Comic Sans MS' },
+    ],
+    toolbarHiddenButtons: [['customClasses'], ['strikeThrough']],
+    customClasses: [
+      {
+        name: 'quote',
+        class: 'quote',
+      },
+      {
+        name: 'redText',
+        class: 'redText',
+      },
+      {
+        name: 'titleText',
+        class: 'titleText',
+        tag: 'h1',
+      },
+    ],
+  };
 
   codeMirrorOptions: any = {
     theme: 'default',
