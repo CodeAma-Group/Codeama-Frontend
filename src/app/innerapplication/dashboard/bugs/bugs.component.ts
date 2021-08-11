3017e498f0b68a13562c23e9bca78bd30f10e28d
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-10 12:50:26 -1200

    ...

--- src/app/innerapplication/dashboard/bugs/bugs.component.ts
+++ src/app/innerapplication/dashboard/bugs/bugs.component.ts
@@ -3,30 +3,75 @@ import { BugService } from '../../services/bug.service';
 @Component({
   selector: 'app-bugs',
   templateUrl: './bugs.component.html',
-  styleUrls: ['./bugs.component.css']
+  styleUrls: ['./bugs.component.css'],
 })
 export class BugsComponent implements OnInit {
-
-  constructor(private bug:BugService) { }
- bugs:any;
-  ngOnInit(): void {
-    this.bug.getBugs().subscribe((result)=>{
-      this.bugs=result;
-      this.bugs=this.bugs.data      
-      console.log(this.bugs);
-    })
-  }
-  checkBadge(badge: string){
-    let className:string = ""
-    switch(badge.toLowerCase()){
-      case "absolute beginner": className = "absBeg";
-        break;
-      case "intermediate": className = "interm";
-        break;
-      case "pro": className = "pro";
-       break;
-      default: className="beginner"
+  constructor(private bug: BugService) {}
+  technologies= [
+    {
+      name: 'vue',
+      img: '../../../../assets/test_images/vue.png'
+    },
+    {
+      name: 'angular',
+      img: '../../../../assets/test_images/angular.png'
+    },
+    {
+      name: 'c',
+      img: '../../../../assets/test_images/c.png'
+    },
+    {
+      name: 'java',
+      img: '../../../../assets/test_images/java.png'
+    },
+    {
+      name: 'react',
+      img: '../../../../assets/test_images/react.png'
+    },
+    {
+      name: 'js',
+      img: '../../../../assets/test_images/js.png'
+    },
+    {
+      name: 'python',
+      img: '../../../../assets/test_images/python.png'
+    },
+    {
+      name: 'swift',
+      img: '../../../../assets/test_images/swift.png'
     }
-    return className
+  ];
+  techImage: string;
+  checkTechImage(name) {
+    for (let i = 0; i < this.technologies.length; i++) {
+      if (this.technologies[i].name == name) {
+        this.techImage = this.technologies[i].img;
+      }
+    }
+  }
+  bugs: any;
+  ngOnInit(): void {
+    this.bug.getBugs().subscribe((result) => {
+      this.bugs = result;
+      this.bugs = this.bugs.data;
+      console.log(this.bugs);
+    });
+  }
+  checkBadge(badge: string) {
+    let className: string = '';
+    switch (badge.toLowerCase()) {
+      case 'absolute beginner':
+        className = 'absBeg';
+        break;
+      case 'intermediate':
+        className = 'interm';
+        break;
+      case 'pro':
+        className = 'pro';
+        break;
+      default:
+        className = 'beginner';
+    }
+    return className;
   }
 }
