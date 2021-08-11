5f4517b1ad659ff1376fb2ccbfbaf39388bd35ea
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-10 13:08:15 -1200

    technologies prob solved

--- src/app/innerapplication/dashboard/bugs/bugs.component.ts
+++ src/app/innerapplication/dashboard/bugs/bugs.component.ts
@@ -7,47 +7,48 @@ import { BugService } from '../../services/bug.service';
 })
 export class BugsComponent implements OnInit {
   constructor(private bug: BugService) {}
-  technologies= [
+  technologies = [
     {
       name: 'vue',
-      img: '../../../../assets/test_images/vue.png'
+      img: '../../../../assets/test_images/vue.png',
     },
     {
       name: 'angular',
-      img: '../../../../assets/test_images/angular.png'
+      img: '../../../../assets/test_images/angular.png',
     },
     {
       name: 'c',
-      img: '../../../../assets/test_images/c.png'
+      img: '../../../../assets/test_images/c.png',
     },
     {
       name: 'java',
-      img: '../../../../assets/test_images/java.png'
+      img: '../../../../assets/test_images/java.png',
     },
     {
       name: 'react',
-      img: '../../../../assets/test_images/react.png'
+      img: '../../../../assets/test_images/react.png',
     },
     {
       name: 'js',
-      img: '../../../../assets/test_images/js.png'
+      img: '../../../../assets/test_images/js.png',
     },
     {
       name: 'python',
-      img: '../../../../assets/test_images/python.png'
+      img: '../../../../assets/test_images/python.png',
     },
     {
       name: 'swift',
-      img: '../../../../assets/test_images/swift.png'
-    }
+      img: '../../../../assets/test_images/swift.png',
+    },
   ];
   techImage: string;
-  checkTechImage(name) {
+  checkTechImage(name: string) {
     for (let i = 0; i < this.technologies.length; i++) {
       if (this.technologies[i].name == name) {
         this.techImage = this.technologies[i].img;
       }
     }
+    return this.techImage;
   }
   bugs: any;
   ngOnInit(): void {
