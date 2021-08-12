74f9cfd813504737065127278ab15d0f7bb8698d
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-12 03:49:45 -1200

    added two new components

---
+++ src/app/innerapplication/take-challenge-board/take-challenge-board.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-take-challenge-board',
+  templateUrl: './take-challenge-board.component.html',
+  styleUrls: ['./take-challenge-board.component.css']
+})
+export class TakeChallengeBoardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
