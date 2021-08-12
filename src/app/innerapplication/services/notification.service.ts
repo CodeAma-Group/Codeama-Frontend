16ad25e6a48db77363250a7484382f51fad18460
Esther Umuhoza <eumuhoza83@gmail.com>
2021-08-12 12:29:06 +0200

    Addition of api

---
+++ src/app/innerapplication/services/notification.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import jwt_decode from 'jwt-decode';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor( private http: HttpClient ) { }
+
+	token: string = localStorage.getItem('codeama_auth_token');
+	userData: any = jwt_decode(this.token)
+	userId: string = this.userData._id;
+
+
+  getNotifications(){
+	return this.http.get(`https://codeama-backend.herokuapp.com/notifications/${this.userId}`)
+  }
+
+}
