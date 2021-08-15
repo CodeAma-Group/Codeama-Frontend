c7490d136eab29aebc7393024aa1678ec5821425
spfvtnjr <74837665+spfvtnjr@users.noreply.github.com>
2021-08-15 00:07:26 -1200

    added user profile to challenge-taker-board

--- src/app/innerapplication/take-challenge-board/take-challenge-board.component.ts
+++ src/app/innerapplication/take-challenge-board/take-challenge-board.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../services/user.service';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-take-challenge-board',
   templateUrl: './take-challenge-board.component.html',
-  styleUrls: ['./take-challenge-board.component.css']
+  styleUrls: ['./take-challenge-board.component.css'],
 })
 export class TakeChallengeBoardComponent implements OnInit {
-
-  constructor() { }
-
+  constructor(private user: UserService, private router: ActivatedRoute) {}
+  userData: any = [];
   ngOnInit(): void {
+    this.user.getUserById(this.router.snapshot.params.id).subscribe((res) => {
+      console.log(res);
+      this.userData = res;
+    });
   }
-
+  checkBadge(badge: string) {
+    let className: string = '';
+    switch (badge.toLowerCase()) {
+      case 'absolute beginner':
+        className = 'absBeg';
+        break;
+      case 'intermediate':
+        className = 'interm';
+        break;
+      case 'pro':
+        className = 'pro';
+        break;
+      default:
+        className = 'beginner';
+    }
+    return className;
+  }
 }
