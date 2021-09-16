a173e0668228e9982351eae5eb19999b134756a7
abi-seth <abiheloaf@gmail.com>
2021-09-16 11:06:58 +0200

    Fix: fixed some errors

--- src/app/innerapplication/userprofile/userresources/userresources.component.ts
+++ src/app/innerapplication/userprofile/userresources/userresources.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from '../../services/user.service'
+import { Router, ActivatedRoute} from '@angular/router';
+import { AppsettingsService } from '../../../appsettings.service'
+import { AuthService } from '../../../authentication/_authServices/auth.service'
+import jwt_decode from 'jwt-decode'
+import { InnerapplicationService } from '../../innerapplication.service';
 @Component({
   selector: 'app-userresources',
   templateUrl: './userresources.component.html',
-  styleUrls: ['./userresources.component.css']
+  styleUrls: ['./userresources.component.scss']
 })
 export class UserresourcesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private backendService: InnerapplicationService, private activatedRoute: ActivatedRoute, private authService: AuthService, private _settings: AppsettingsService, private _userService: UserService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
+	articles: any;
+	_id: string;
+  
+	ngOnInit(): void {
+		
+    let token:any = localStorage.getItem('codeama_auth_token');
+		let user:any = jwt_decode(token);
+
+		this._id = user._id;
+
+    // this._id = this.activatedRoute.snapshot.paramMap.get('user');
+    // console.error(this._id)
+		this._settings.settings();
+	
+		this.backendService.getQuestions().subscribe((data: any[]) => {
+			this.articles = data
+		})
+	}
 
 }
