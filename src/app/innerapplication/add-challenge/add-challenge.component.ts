103687916388d7f5a9664662e92f169dce48e487
gasaroleila@gmail.com <uwamgaleila@gmail.com>
2021-09-17 21:31:31 +0200

    fixed post challenge page

--- src/app/innerapplication/add-challenge/add-challenge.component.ts
+++ src/app/innerapplication/add-challenge/add-challenge.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms';
+import jwt_decode from 'jwt-decode'
+
 
 @Component({
   selector: 'app-add-challenge',
@@ -14,7 +16,11 @@ export class AddChallengeComponent implements OnInit {
     this.challenge = this.formBuilder.group({
       title: ['', [Validators.required]],
       description: ['',[Validators.required]],
-      challengeImg: ['',[Validators.required]]
+      challengeImg: ['',[Validators.required]],
+      prizes: this.formBuilder.array([]),
+      applicationEndDate: ['', Validators.required],
+      endDate: ['', Validators.required],
+      announceWinnersDate: ['', Validators.required]
     })    
    }
 
@@ -31,8 +37,35 @@ export class AddChallengeComponent implements OnInit {
     fileReader.readAsDataURL(file)
   }
 
-  submitChallenge(e: Event) {
-    console.log(e)
+  setDate() {
+    return new Date(Date.now())
   }
+
+  get prizes():FormArray {
+    return this.challenge.get('prizes') as FormArray
+  }
+
+  submitChallenge(e: Event) {
+    e.preventDefault()
+    // var decoded: any = jwt_decode(localStorage.codeama_auth_token)
+    let newChallenge: FormData = new FormData()
+
+    console.log('prizes',this.prizes.value)
+
+    newChallenge.append('title', this.challenge.value.title)
+    newChallenge.append('description', this.challenge.value.description)
+    newChallenge.append('picture', this.challenge.value.challengeImg)
+    newChallenge.append('prizes', this.challenge.value.prizes)
+    newChallenge.append('applicationEndDate', this.challenge.value.applicationEndDate)
+    newChallenge.append('endDate', this.challenge.value.endDate)
+    newChallenge.append('announceWinnersDate', this.challenge.value.announceWinnersDate)
+    newChallenge.append('random', 'this is some text')
+
+    for(var pair of newChallenge.entries()) {
+      console.log(pair[0]+ ', '+ pair[1]);
+   }
+  }
+
+
 
 }
