e8adde85b00f378885a0be1bb0d0e02718698e51
abi-seth <abiheloaf@gmail.com>
2021-10-17 20:00:51 +0200

    Fix: fixed errors in updating the profile

--- src/app/innerapplication/profile/profile.component.ts
+++ src/app/innerapplication/profile/profile.component.ts
@@ -193,16 +193,16 @@ export class ProfileComponent implements OnInit {
   updateProfileToDb(data) {
     this._userService.updateProfile(data).subscribe(
 			res => {
-        console.log(res)
         this.editingMinorAccountSettings = false;
         this.editingAccountSettings = false;
         this.notifier.notify("success", this.success_msg) 
         this.hasSubmitted = false;       
         this.editingAccountSettings = false;
-        this.router.navigate(['/app/profile/', this._id])
+        // this.router.navigate(['/app/profile/', this._id])
+
+        location.reload();
       },
 			err => {
-        console.error(err)
         this.hasSubmitted = false; 
         this.hasSubmittedMinorChanges = false;      
         this.notifier.notify("error", this.error_msg);        
