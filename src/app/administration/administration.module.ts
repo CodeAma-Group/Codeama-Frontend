25565d9ab5f88e2b9fbc0b55d7e82a4a4f112268
gasaroleila@gmail.com <uwamgaleila@gmail.com>
2021-10-30 10:21:33 +0200

    admin dashboard

--- src/app/administration/administration.module.ts
+++ src/app/administration/administration.module.ts
@@ -10,10 +10,15 @@ import { ModuleoutletComponent } from './moduleoutlet/moduleoutlet.component';
 import { AdminGroupComponent } from './admin-group/admin-group.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { HeaderComponent } from './header/header.component';
+import { CardBoxComponent } from './dashboard/card-box/card-box.component';
+import { ActivitiesBoxComponent } from './dashboard/activities-box/activities-box.component';
+import { OtherInfoComponent } from './dashboard/other-info/other-info.component';
+import { GraphComponent } from './dashboard/graph/graph.component';
+import { AmaItemComponent } from './dashboard/other-info/ama-item/ama-item.component';
 
 
 @NgModule({
-  declarations: [SidebarComponent, DashboardComponent, OurUsersComponent, CodeamasComponent, ModuleoutletComponent, AdminGroupComponent, NotificationsComponent, HeaderComponent],
+  declarations: [SidebarComponent, DashboardComponent, OurUsersComponent, CodeamasComponent, ModuleoutletComponent, AdminGroupComponent, NotificationsComponent, HeaderComponent, CardBoxComponent, ActivitiesBoxComponent, OtherInfoComponent, GraphComponent, AmaItemComponent],
   imports: [
     CommonModule,
     AdministrationRoutingModule
--- src/app/administration/dashboard/dashboard.component.ts
+++ src/app/administration/dashboard/dashboard.component.ts
@@ -14,4 +14,55 @@ export class DashboardComponent implements OnInit {
   componentText = "Dashboard works!";
   heading = "Administration dashboard";
 
+  cards = [
+    {
+      title: "CodeAmas",
+      totalCount: "7,345",
+      icon: "../../assets/icons/Group 67.png"
+    },
+
+    {
+      title: "Bugs Published",
+      totalCount: "7,345",
+      icon: "../../assets/icons/Group 68.png"
+    },
+
+    {
+      title: "Resources",
+      totalCount: "7,345",
+      icon: "../../assets/icons/Group 69.png"
+    },
+
+    {
+      title: "Articles",
+      totalCount: "7,345",
+      icon: "../../assets/icons/Group 70.png"
+    },
+
+    {
+      title: "Other questions",
+      totalCount: "7,345",
+      icon: "../../assets/icons/Group 71.png"
+    }
+  ]
+
+  
+  activities = [
+    {
+      icon: "../../assets/icons/Group 72.png",
+      activity: "Challenges",
+      startDate: "12th January 2021",
+      stats: ["12,345","1000","345"]
+    },
+
+    {
+      icon: "../../assets/icons/Group 73.png",
+      activity: "Projects",
+      startDate: "14th January 2021",
+      stats: ["12,345","100"]
+    }
+  ]
+
+  
+
 }
