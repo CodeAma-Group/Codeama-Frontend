c706a7ad52fdaf55184dd50de537cc99a40c152b
Ineza Jöst <jinezachance@gmail.com>
2021-11-16 18:09:03 +0200

    Added codeama table and finalized user tables

--- src/app/administration/admin-group/admin-group.component.ts
+++ src/app/administration/admin-group/admin-group.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/innerapplication/services/user.service';
+import { AdministrationService } from '../administration.service';
 
 @Component({
   selector: 'app-admin-group',
@@ -7,11 +9,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminGroupComponent implements OnInit {
 
-  constructor() { }
-
+ 
+  constructor(private administrationService: AdministrationService, private userService: UserService) { }
+  userProfileData: String;
+  users: any = [];
+  loader: boolean = true;
+  selectedUser:boolean = true;
   ngOnInit(): void {
+    this.getAllUsers();
+    this.badgeHolderNumber("beginner");
   }
-  componentText = "Admin Group Works!";
-  heading = "Codeama admin users"
+
+  getAllUsers() {
+    this.administrationService.getAllUsers().subscribe(res => {
+      this.users = res.data;
+      console.log(res.data);
+      this.loader = false;
+    })
+  }
+  checkBadge(badge: string) {
+    console.log("Here")
+    console.log(badge);
+    let className: string = ""
+    switch (badge.toLowerCase()) {
+      case "absolute beginner": className = "absBeg";
+        break;
+      case "intermediate": className = "interm";
+        break;
+      case "pro": className = "pro";
+        break;
+      case "admin": className = "admin"; break;
+      case "beginner": className = "beginner"; break;
+    }
+    return className
+  }
+  badgeHolderNumber(badge: string){
+    let count:number = 0;
+    console.log(this.users.Badge);
+    for(let i=0; i<this.users.length; i++){
+        if(this.users[i].Badge == badge){
+          count++;
+        }
+    }
+    console.log(count);
+    return count;
+  }
+  allUsersNumber(){
+    let usersTotal = this.users.length;
+    return usersTotal;
+  }
+  p: number = 1;
+  componentText = "our users works!"
+  heading = "Codeama users"
 
 }
--- src/app/administration/codeamas/codeamas.component.ts
+++ src/app/administration/codeamas/codeamas.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from 'src/app/innerapplication/services/user.service';
+import { AdministrationService } from '../administration.service';
 @Component({
   selector: 'app-codeamas',
   templateUrl: './codeamas.component.html',
@@ -7,11 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CodeamasComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private administrationService: AdministrationService, private userService: UserService) { }
+  userProfileData: String;
+  users: any = [];
+  loader: boolean = true;
+  selectedUser:boolean = true;
   ngOnInit(): void {
+    this.getAllCodeamas();
+    this.badgeHolderNumber("beginner");
   }
-  componentText = "codeamas works!";
-  heading = "Codeama amas";
+
+  getAllCodeamas() {
+    this.administrationService.getAllCodeamas().subscribe(res => {
+      this.users = res.data;
+      console.log(res.data);
+      this.loader = false;
+    })
+  }
+  count(followArr: []){
+    let count;
+    count = followArr.length;
+    return count;
+  }
+  checkBadge(badge: string) {
+    console.log("Here")
+    console.log(badge);
+    let className: string = ""
+    switch (badge.toLowerCase()) {
+      case "absolute beginner": className = "absBeg";
+        break;
+      case "intermediate": className = "interm";
+        break;
+      case "pro": className = "pro";
+        break;
+      case "admin": className = "admin"; break;
+      case "beginner": className = "beginner"; break;
+    }
+    return className
+  }
+  badgeHolderNumber(badge: string){
+    let count:number = 0;
+    console.log(this.users.Badge);
+    for(let i=0; i<this.users.length; i++){
+        if(this.users[i].Badge == badge){
+          count++;
+        }
+    }
+    console.log(count);
+    return count;
+  }
+  allUsersNumber(){
+    let usersTotal = this.users.length;
+    return usersTotal;
+  }
+  p: number = 1;
+  componentText = "our users works!"
+  heading = "Codeama users"
 
 }
