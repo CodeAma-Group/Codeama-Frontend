9b182baf3d0068d926a8ea55c37064aed8b7c0eb
gasaroleila <uwamgaleila@gmail.com>
2022-01-19 21:56:57 +0200

    add challenge

--- src/app/innerapplication/add-challenge/add-badge/add-badge.component.ts
+++ src/app/innerapplication/add-challenge/add-badge/add-badge.component.ts
@@ -10,8 +10,9 @@ export class AddBadgeComponent implements OnInit {
   marksInput: any = document.getElementById('marks')
   @Input() prizeIndex:any
   @Input() status:string
-  @Input() prizes: {}
+  @Input() prizes: any
   @Output() marks = new EventEmitter<Number>()
+  @Output() prizeStatuses = new EventEmitter<String>()
 
 
   addMarks(value: Number) {
@@ -21,6 +22,21 @@ export class AddBadgeComponent implements OnInit {
     }
     this.marks.emit(value)
     this.marksInput.blur()
+  }
+
+  addStatus(value: String) {
+    if(value === null || value === undefined) {
+      alert('Please enter value')
+      return 
+    }
+    this.prizeStatuses.emit(value)
+  }
+
+  removePrize() {
+    if(this.prizes) {
+      this.prizes = this.prizes.filter((value, index, array) => { return value.status === 'admin' })
+      console.log(this.prizes)
+    }
   }
   constructor() { }
 
